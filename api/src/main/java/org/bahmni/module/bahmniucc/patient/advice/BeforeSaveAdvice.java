875ac0ef91c237e6891b8ac1fc12e01ff2db6705
Kaustav Chakraborty <kaustav.ron@gmail.com>
2017-07-24 11:47:49 +0300

    Kaustav | #0000 | Fixed before save advice for new patient not getting registered

--- api/src/main/java/org/bahmni/module/bahmniucc/patient/advice/BeforeSaveAdvice.java
+++ api/src/main/java/org/bahmni/module/bahmniucc/patient/advice/BeforeSaveAdvice.java
@@ -3,6 +3,7 @@ package org.bahmni.module.bahmniucc.patient.advice;
 import org.bahmni.module.bahmniucc.patient.validations.PatientDeathValidation;
 import org.bahmni.module.bahmniucc.patient.validations.UCCValidationRules;
 import org.openmrs.Patient;
+import org.openmrs.api.APIException;
 import org.springframework.aop.MethodBeforeAdvice;
 
 import java.lang.reflect.Method;
@@ -12,11 +13,13 @@ public class BeforeSaveAdvice implements MethodBeforeAdvice {
     private static final String SAVE_PATIENT_METHOD_TO_INTERCEPT = "savePatient";
 
     @Override
-    public void before(Method method, Object[] objects, Object o) throws Throwable {
+    public void before(Method method, Object[] objects, Object o) {
         if (method.getName().equalsIgnoreCase(SAVE_PATIENT_METHOD_TO_INTERCEPT)) {
             Patient patient = (Patient) objects[0];
-            UCCValidationRules patientDeathValidation = new PatientDeathValidation(patient);
-            patientDeathValidation.execute();
+            if (patient.getPersonId() != null) {
+                UCCValidationRules patientDeathValidation = new PatientDeathValidation(patient);
+                patientDeathValidation.execute();
+            }
         }
     }
 }
